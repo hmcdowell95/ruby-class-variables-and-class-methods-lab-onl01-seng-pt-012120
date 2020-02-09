39dcc77772e91e9071dafaec67592c3a2dacf9cd
Harley McDowell <hmcdowell95@gmail.com>
2020-02-09 03:19:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,6 +9,9 @@ class Song
  
   def initialize(name, artist, genre)
     @@count += 1
+    @name = name
+    @artist = artist
+    @genre = genre
     @@genres << genre
     @@artists << artist
   end
